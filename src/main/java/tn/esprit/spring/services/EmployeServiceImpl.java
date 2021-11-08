4f42b070755fed1638fe7a21240324794f415aa3
fareselheni <fares.elheni1@esprit.tn>
2021-11-08 21:34:08 +0100

    add timesheet_service

--- src/main/java/tn/esprit/spring/services/EmployeServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EmployeServiceImpl.java
@@ -1,28 +1,23 @@
 package tn.esprit.spring.services;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import tn.esprit.spring.entities.Contrat;
-import tn.esprit.spring.entities.Departement;
-import tn.esprit.spring.entities.Employe;
-import tn.esprit.spring.entities.Entreprise;
-import tn.esprit.spring.entities.Mission;
-import tn.esprit.spring.entities.Timesheet;
+import tn.esprit.spring.entities.*;
 import tn.esprit.spring.repository.ContratRepository;
 import tn.esprit.spring.repository.DepartementRepository;
 import tn.esprit.spring.repository.EmployeRepository;
 import tn.esprit.spring.repository.TimesheetRepository;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class EmployeServiceImpl implements IEmployeService {
+
 	public static final Logger logger = Logger.getLogger(EmployeServiceImpl.class);
 
 	@Autowired
@@ -36,81 +31,183 @@ public class EmployeServiceImpl implements IEmployeService {
 
 	//firas chbinou
 	public int ajouterEmploye(Employe employe) {
-		employeRepository.save(employe);
+		try {
+			logger.info("In ajouterEmploye():");
+			logger.debug("debut d'ajout de l'employe: " + employe.getNom());
+			employeRepository.save(employe);
+			logger.info("out of ajouterEmploye()");
+			logger.debug("l'employe: " + employe.getNom() + " de l'id: " + employe.getId() + " ajoutée avec succé");
+		}catch(Exception e){
+			logger.error("erreur a ajouterEmploye(): "+e);
+		}
 		return employe.getId();
 	}
 
-	public void mettreAjourEmailByEmployeId(String email, int employeId) {
-		Employe employe = employeRepository.findById(employeId).get();
-		employe.setEmail(email);
-		employeRepository.save(employe);
+	public int mettreAjourEmailByEmployeId(String email, int employeId) {
+		int id =0;
+		Optional<Employe> value = this.employeRepository.findById(employeId);
+		Employe employe=new Employe();
+		try {
+			logger.info("In mettreAjourEmailByEmployeId()");
+			if(value.isPresent()){
+				employe = value.get();
+			}
+			logger.debug("debut de modification de l'email de l'employe: "+employe.getNom());
+			employe.setEmail(email);
+			employeRepository.save(employe);
+			logger.info("Out of mettreAjourEmailByEmployeId()");
+			logger.debug("l'employe: "+employe.getNom()+" de l'id: "+employe.getId()+" a été modifié");
+			if(value.isPresent()){
+				id=value.get().getId();
+			}
+		}catch(Exception e){
+			logger.error("erreur a mettreAjourEmailByEmployeId: "+e);
+		}
+		return id;
 
 	}
 
 	@Transactional	
-	public void affecterEmployeADepartement(int employeId, int depId) {
-		Departement depManagedEntity = deptRepoistory.findById(depId).get();
-		Employe employeManagedEntity = employeRepository.findById(employeId).get();
+	public int affecterEmployeADepartement(int employeId, int depId) {
+		logger.info("In affecterEmployeADepartement()");
+		logger.debug("début de l'affectation de l'emplye de l'id: "+employeId+" au departement de l'id: "+depId);
+		Optional<Departement> depManaged = deptRepoistory.findById(depId);
+		Optional<Employe> employeManaged = employeRepository.findById(employeId);
+		int id = 0;
+		try {
 
-		if(depManagedEntity.getEmployes() == null){
+			Departement depManagedEntity = new Departement();
+			Employe employeManagedEntity =new Employe();
+			if(depManaged.isPresent()){
+				depManagedEntity=depManaged.get();
+			}
+			if(employeManaged.isPresent()){
+				 employeManagedEntity = employeManaged.get();
+			}
 
-			List<Employe> employes = new ArrayList<>();
-			employes.add(employeManagedEntity);
-			depManagedEntity.setEmployes(employes);
-		}else{
 
-			depManagedEntity.getEmployes().add(employeManagedEntity);
+			if (depManagedEntity.getEmployes() == null) {
 
+				List<Employe> employes = new ArrayList<>();
+				employes.add(employeManagedEntity);
+				depManagedEntity.setEmployes(employes);
+			} else {
+
+				depManagedEntity.getEmployes().add(employeManagedEntity);
+
+			}
+			logger.info("Out of affecterEmployeADepartement()");
+			logger.debug("l'affectation a été effectué avec succé");
+		}catch(Exception e){
+			logger.error("erreur a affecterEmployeADepartement(): "+e);
 		}
+		if(employeManaged.isPresent()){
+			id=employeManaged.get().getId();
+		}
+		return id;
 
 	}
-	@Transactional
-	public void desaffecterEmployeDuDepartement(int employeId, int depId)
-	{
-		Departement dep = deptRepoistory.findById(depId).get();
 
-		int employeNb = dep.getEmployes().size();
-		for(int index = 0; index < employeNb; index++){
-			if(dep.getEmployes().get(index).getId() == employeId){
-				dep.getEmployes().remove(index);
-				break;//a revoir
+	@Transactional
+	public int desaffecterEmployeDuDepartement(int employeId, int depId)
+	{
+		logger.info("In desaffecterEmployeDuDepartement()");
+		logger.debug("début de la desaffectation de l'emplye de l'id: "+employeId+" au departement de l'id: "+depId);
+		Optional<Departement> departement = deptRepoistory.findById(depId);
+		Departement dep = new Departement();
+		try {
+			if(departement.isPresent()){
+				dep = departement.get();
 			}
+
+
+			int employeNb = dep.getEmployes().size();
+			for (int index = 0; index < employeNb; index++) {
+				if (dep.getEmployes().get(index).getId() == employeId) {
+					dep.getEmployes().remove(index);
+					break;//a revoir
+				}
+			}
+			logger.info("Out of desaffecterEmployeDuDepartement()");
+			logger.debug("la desaffectation a été effectué avec succé");
+		}catch(Exception e){
+			logger.error("erreur a desaffecterEmployeDuDepartement(): "+e);
 		}
+		return dep.getId();
 	}
 
 	public int ajouterContrat(Contrat contrat) {
-		contratRepoistory.save(contrat);
+		try{
+			logger.info("in ajouterContrat()");
+			logger.debug("début d'ajout du contrat num: "+contrat.getReference());
+			contratRepoistory.save(contrat);
+			logger.info("Out of ajouterContrat()");
+			logger.debug("fin d'ajout du contrat num: "+contrat.getReference());
+		}catch(Exception e){
+			logger.error("erreur a ajouterContrat(): "+e);
+		}
+
 		return contrat.getReference();
 	}
 
-	public void affecterContratAEmploye(int contratId, int employeId) {
-		Contrat contratManagedEntity = contratRepoistory.findById(contratId).get();
-		Employe employeManagedEntity = employeRepository.findById(employeId).get();
-
-		contratManagedEntity.setEmploye(employeManagedEntity);
-		contratRepoistory.save(contratManagedEntity);
+	public int affecterContratAEmploye(int contratId, int employeId) {
+		logger.info("in affecterContratAEmploye()");
+		logger.debug("début d'affectation du contrat num: "+contratId +" a l'emplye: "+employeId);
+		Contrat contratManagedEntity = new Contrat();
+		Employe employeManagedEntity = new Employe();
+		Optional<Contrat> contrat = contratRepoistory.findById(contratId);
+		Optional<Employe> employe = employeRepository.findById(contratId);
+		int id=0;
+		try{
+			if(contrat.isPresent()){
+				contratManagedEntity = contrat.get();
+			}
+			if(employe.isPresent()){
+				 employeManagedEntity = employe.get();
+			}
+			contratManagedEntity.setEmploye(employeManagedEntity);
+			contratRepoistory.save(contratManagedEntity);
+			logger.info("Out of affecterContratAEmploye()");
+			logger.debug("fin d'affectation du contrat num: "+contratId +" a l'emplye: "+employeId);
+		}catch(Exception e){
+			logger.error("erreur a affecterContratAEmploye(): "+e);
+		}
+		if(contrat.isPresent()){
+			id = contrat.get().getReference();
+		}
+		return id;
 		
 	}
 
 
 	public String getEmployePrenomById(int employeId) {
-		Employe employeManagedEntity = employeRepository.findById(employeId).get();
+		Employe employeManagedEntity=new Employe();
+		Optional<Employe> emp = employeRepository.findById(employeId);
+		try{
+			logger.info("in getEmployePrenomById()");
+			logger.debug("debut de l'opperation");
+			if(emp.isPresent()){
+				employeManagedEntity = emp.get();
+			}
+
+			logger.info("Out of getEmployePrenomById()");
+			logger.debug("employe récupéré sans echec: "+employeManagedEntity.getPrenom());
+
+		}catch(Exception e) {
+			logger.error("erreur a getEmployePrenomById(): " + e);
+		}
 		return employeManagedEntity.getPrenom();
 	}
 
 
 	//fares
-	
-
 	public void deleteEmployeById(int employeId)
 	{
 		Optional<Employe> employe = employeRepository.findById(employeId);
 		
 		
 
-		//Desaffecter l'employe de tous les departements
-		//c'est le bout master qui permet de mettre a jour
-		//la table d'association
+		
 		if(employe.isPresent()) {
 			logger.info("In deleteEmployeById():");
 			logger.debug("debut d'effacement de l'employe: " +  employe.get().getNom());
@@ -182,7 +279,7 @@ public class EmployeServiceImpl implements IEmployeService {
 	public Double getSalaireMoyenByDepartementId(int departementId) {
 		return employeRepository.getSalaireMoyenByDepartementId(departementId);
 	}
-	
+
 	public List<Timesheet> getTimesheetsByMissionAndDate(Employe employe, Mission mission, Date dateDebut,
 			Date dateFin) {
 		return timesheetRepository.getTimesheetsByMissionAndDate(employe, mission, dateDebut, dateFin);
--- src/main/java/tn/esprit/spring/services/TimesheetServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/TimesheetServiceImpl.java
@@ -1,27 +1,22 @@
 package tn.esprit.spring.services;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import tn.esprit.spring.entities.Departement;
-import tn.esprit.spring.entities.Employe;
-import tn.esprit.spring.entities.Mission;
-import tn.esprit.spring.entities.Role;
-import tn.esprit.spring.entities.Timesheet;
-import tn.esprit.spring.entities.TimesheetPK;
+import tn.esprit.spring.entities.*;
 import tn.esprit.spring.repository.DepartementRepository;
 import tn.esprit.spring.repository.EmployeRepository;
 import tn.esprit.spring.repository.MissionRepository;
 import tn.esprit.spring.repository.TimesheetRepository;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class TimesheetServiceImpl implements ITimesheetService {
-	
+	public static final Logger logger = Logger.getLogger(TimesheetServiceImpl.class);
 
 	@Autowired
 	MissionRepository missionRepository;
@@ -34,39 +29,59 @@ public class TimesheetServiceImpl implements ITimesheetService {
 
 	//rayen
 	public int ajouterMission(Mission mission) {
+		logger.info("In ajouterMission():");
+		logger.debug("debut d'ajout de la mission: " + mission.getName());
 		missionRepository.save(mission);
+		logger.info("out of ajouterMission()");
+		logger.debug("la mission: " + mission.getName() + " de l'id: " + mission.getId() + " ajoutée avec succé");
 		return mission.getId();
 	}
-    
+
 	public void affecterMissionADepartement(int missionId, int depId) {
-		Mission mission = missionRepository.findById(missionId).get();
-		Departement dep = deptRepoistory.findById(depId).get();
+		logger.info("In affecterMissionADepartement():");
+		logger.debug("debut d'ajout de la mission: " + missionId );
+		Optional<Mission> missionD = missionRepository.findById(missionId);
+		Optional<Departement> depD = deptRepoistory.findById(depId);
+		if (missionD.isPresent() && depD.isPresent()){
+		Mission mission = missionD.get();
+		Departement dep = depD.get();
 		mission.setDepartement(dep);
 		missionRepository.save(mission);
-	}
+		logger.info("out of affecterMissionADepartement()");
+		logger.debug("la mission: " + missionId + " est affectée avec succé au département" + depId);}
+}
 
 	public void ajouterTimesheet(int missionId, int employeId, Date dateDebut, Date dateFin) {
+		logger.info("In ajouterTimesheet():");
+		logger.debug("debut d'ajout de TimeSheet: " + missionId + employeId + dateDebut + dateFin );
 		TimesheetPK timesheetPK = new TimesheetPK();
-		timesheetPK.setDateDebut(dateDebut);
-		timesheetPK.setDateFin(dateFin);
 		timesheetPK.setIdEmploye(employeId);
 		timesheetPK.setIdMission(missionId);
+		timesheetPK.setDateDebut(dateDebut);
+		timesheetPK.setDateFin(dateFin);
 		
 		Timesheet timesheet = new Timesheet();
 		timesheet.setTimesheetPK(timesheetPK);
 		timesheet.setValide(false); //par defaut non valide
 		timesheetRepository.save(timesheet);
+		logger.info("out of ajouterTimesheet()");
+		logger.debug("Le Timesheet" + missionId + employeId + dateDebut + dateFin + "est ajouté avec succès");
+		
 		
 	}
 
-	
+
 	public void validerTimesheet(int missionId, int employeId, Date dateDebut, Date dateFin, int validateurId) {
-		System.out.println("In valider Timesheet");
-		Employe validateur = employeRepository.findById(validateurId).get();
-		Mission mission = missionRepository.findById(missionId).get();
+		logger.info("In validerTimesheet():");
+		logger.debug("debut de validation de timesheet: " + missionId + employeId + dateDebut + dateFin );
+		Optional<Employe> validateurD = employeRepository.findById(validateurId);
+		Optional<Mission> missD = missionRepository.findById(missionId);
+		if (validateurD.isPresent() && missD.isPresent()){
+		Employe validateur = validateurD.get();
+		Mission mission = missD.get();
 		//verifier s'il est un chef de departement (interet des enum)
 		if(!validateur.getRole().equals(Role.CHEF_DEPARTEMENT)){
-			System.out.println("l'employe doit etre chef de departement pour valider une feuille de temps !");
+			logger.info("l'employe doit etre chef de departement pour valider une feuille de temps !");
 			return;
 		}
 		//verifier s'il est le chef de departement de la mission en question
@@ -78,7 +93,7 @@ public class TimesheetServiceImpl implements ITimesheetService {
 			}
 		}
 		if(!chefDeLaMission){
-			System.out.println("l'employe doit etre chef de departement de la mission en question");
+			logger.info("l'employe doit etre chef de departement de la mission en question");
 			return;
 		}
 //
@@ -88,18 +103,34 @@ public class TimesheetServiceImpl implements ITimesheetService {
 		
 		//Comment Lire une date de la base de données
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		System.out.println("dateDebut : " + dateFormat.format(timesheet.getTimesheetPK().getDateDebut()));
-		
+		logger.info("out of validerTimesheet()");
+		logger.info("dateDebut : " + dateFormat.format(timesheet.getTimesheetPK().getDateDebut()));
+		logger.debug("Le Timesheet" + missionId + employeId + dateDebut + dateFin + "est valide");}
 	}
 
-	
-	public List<Mission> findAllMissionByEmployeJPQL(int employeId) {
-		return timesheetRepository.findAllMissionByEmployeJPQL(employeId);
+
+	public List<Mission> findAllMissionByEmployeJPQL(int employeId) { 
+		logger.info("In findAllMissionByEmployeJPQL():");
+		logger.debug("debut de recherche des Missions de l'employe avec l'id: " + employeId  );
+		List <Mission> missionss =timesheetRepository.findAllMissionByEmployeJPQL(employeId);
+		logger.info("out of findAllMissionByEmployeJPQL()");
+		logger.debug("L'employe " + employeId + "à la liste" + missionss);
+		return missionss;
+		
+		
+		
+
 	}
 
 	
 	public List<Employe> getAllEmployeByMission(int missionId) {
-		return timesheetRepository.getAllEmployeByMission(missionId);
+		logger.info("In getAllEmployeByMission():");
+		logger.debug("debut de recherche des employes par mission: " + missionId  );
+		
+		List <Employe> employes =timesheetRepository.getAllEmployeByMission(missionId);
+		logger.info("out of getAllEmployeByMission()");
+		logger.debug("Les employes " + employes + "sont affectés à la mission" + missionId);
+		return employes;
 	}
 
 }
