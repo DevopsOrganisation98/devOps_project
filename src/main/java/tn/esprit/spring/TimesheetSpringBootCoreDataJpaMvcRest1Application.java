1d6099091740ad0f952a5a95cb997e83bd8091e7
firas chbinou <chbinoumedfiras@gmail.com>
2021-11-08 12:08:06 +0100

    test 24

--- src/main/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1Application.java
+++ src/main/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1Application.java
@@ -1,13 +1,12 @@
 package tn.esprit.spring;
 
 
-import org.apache.log4j.PropertyConfigurator;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.nio.file.Paths;
 
 @SpringBootApplication
 public class TimesheetSpringBootCoreDataJpaMvcRest1Application {
